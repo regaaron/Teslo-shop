af1d503ed4f7f0a857ddf224426d677ed79fa402
aaron lopez <aaron_lopez222@hotmail.com>
2026-05-05 17:10:15 -0600

    Chech-Status con interceptores

--- src/api/tesloApi.ts
+++ src/api/tesloApi.ts
@@ -8,5 +8,14 @@ const tesloApi = axios.create({
 })
 
 //TODO: interceptores
+tesloApi.interceptors.request.use((config) => {
+
+    const token = localStorage.getItem('token');
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+})
 
 export {tesloApi}
---
+++ src/auth/actions/chech-auth.action.ts
@@ -0,0 +1,19 @@
+import { tesloApi } from "@/api/tesloApi";
+import type { AuthResponse } from "../interface/auth.response";
+
+export const ChechAuthAction = async ():Promise<AuthResponse> =>{
+    const token = localStorage.getItem('token');
+    if(!token) throw new Error('No token found');
+
+    try{
+        const {data} = await tesloApi.get<AuthResponse>('/auth/chech-status');
+        localStorage.setItem('token',token);
+
+        return data;
+    } catch(error){
+        localStorage.removeItem('token');
+        console.log(error);
+        throw new Error('Token expired or not valid');
+        
+    }
+}
