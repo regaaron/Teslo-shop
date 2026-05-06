73315dddb9505e3db0fe993eddddfaf19cba9611
aaron lopez <aaron_lopez222@hotmail.com>
2026-05-06 14:42:03 -0600

    feat: add search functionality to AdminHeader and update AdminProductsPages layout

--- src/admin/components/AdminHeader.tsx
+++ src/admin/components/AdminHeader.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import { Search, Bell, MessageSquare, Settings } from 'lucide-react';
 import { useAuthStore } from '@/auth/store/auth.store';
+import { useSearchParams } from 'react-router';
 
 export const AdminHeader: React.FC = () => {
   const {user} = useAuthStore()
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  const handleSearch = (event: React.KeyboardEvent<HTMLInputElement>)=>{
+    if(event.key !== "Enter") return;
+
+    const query = inputRef.current?.value.trim() || "";
+    const newSearchParams = new URLSearchParams();
+
+    if(!query){
+        newSearchParams.delete("query")
+    }else{
+        newSearchParams.set('query', query);
+    }
+    setSearchParams(newSearchParams);
+  }
+  
   return (
     <header className="bg-white border-b border-gray-200 px-6 py-4 h-18">
       <div className="flex items-center justify-between">
@@ -15,6 +34,8 @@ export const AdminHeader: React.FC = () => {
               type="text"
               placeholder="Search..."
               className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
+              onKeyDown={handleSearch}
+              ref={inputRef}
             />
           </div>
         </div>
--- src/admin/products/AdminProductsPages.tsx
+++ src/admin/products/AdminProductsPages.tsx
@@ -1,72 +1,97 @@
-import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table"
+import {
+  Table,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+} from "@/components/ui/table";
 
-import { AdminTitle } from "../components/AdminTitle"
-import { Button } from "@/components/ui/button"
-import { Link } from "react-router"
-import { CustomPagination } from "@/components/custom/CustomPagination"
-import { PlusIcon } from "lucide-react"
+import { AdminTitle } from "../components/AdminTitle";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { PlusIcon } from "lucide-react";
+import { useProducts } from "@/hooks/useProducts";
 
 export const AdminProductsPages = () => {
-    return (
-        <>
+  const { data } = useProducts();
 
-            <div className="flex justify-between items-center">
-                <AdminTitle
-                    title="Productos"
-                    subtitle="Aqui puedes ver y administrar tus productos"
-                />
+  return (
+    <>
+      <div className="flex justify-between items-center">
+        <AdminTitle
+          title="Productos"
+          subtitle="Aqui puedes ver y administrar tus productos"
+        />
 
-                <div className="flex justify-end mb-10 gap-4">
-                    <Link to='/admin/products/new'>
-                        <Button>
-                            <PlusIcon />
-                            Nuevo Producto
-                        </Button>
-                    </Link>
+        <div className="flex justify-end mb-10 gap-4">
+          <Link to="/admin/products/new">
+            <Button>
+              <PlusIcon />
+              Nuevo Producto
+            </Button>
+          </Link>
+        </div>
+      </div>
 
-                </div>
-            </div>
+      <Table className="bg-white p-10 shadow-xs border border-gray-200 mb-10">
+        <TableHeader>
+          <TableRow>
+            <TableHead>Imagen</TableHead>
+            <TableHead>Nombre</TableHead>
+            <TableHead>Precio</TableHead>
+            <TableHead>Categoria</TableHead>
+            <TableHead>Invetario</TableHead>
+            <TableHead>Tallas</TableHead>
+            <TableHead className="text-right">Acciones</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data?.products.map((product) => {
+            return (
+              <TableRow>
+                <TableCell>
+                  <Link to={`/admin/products/${product.id}`}>
+                    <img
+                      src={product.images[0]}
+                      alt={product.title}
+                      className="w-20 h-20 object-cover rounded-md"
+                    />
+                  </Link>
+                </TableCell>
+                <TableCell>
+                  <Link
+                    to={`/admin/products/${product.id}`}
+                    className="hover:text-blue-700 underline "
+                  >
+                    {product.title}
+                  </Link>
+                </TableCell>
+                <TableCell>${product.price}</TableCell>
+                <TableCell>{product.gender}</TableCell>
+                <TableCell>{product.stock} stock</TableCell>
 
+                <TableCell>{product.sizes.join(", ")}</TableCell>
 
-            <Table className="bg-white p-10 shadow-xs border border-gray-200 mb-10">
+                <TableCell className="text-right">
+                  <Link to={`/admin/products/${product.id}`}>
+                    <Button
+                      variant="outline"
+                      className="bg-blue-700 text-white"
+                      size="sm"
+                    >
+                      Editar
+                    </Button>
+                  </Link>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
 
-                <TableHeader>
-                    <TableRow>
-                        <TableHead className="w-[100px]">ID</TableHead>
-                        <TableHead>Imagen</TableHead>
-                        <TableHead>Nombre</TableHead>
-                        <TableHead>Precio</TableHead>
-                        <TableHead>Categoria</TableHead>
-                        <TableHead>Invetario</TableHead>
-                        <TableHead>Tallas</TableHead>
-                        <TableHead className="text-right">Acciones</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    <TableRow>
-                        <TableCell className="font-medium">1</TableCell>
-                        <TableCell>
-                            <img src="https://placehold.co/250x205" alt="producto"
-                                className="w-20 h-20 object-cover rounded-md" />
-                        </TableCell>
-                        <TableCell>Producto 1</TableCell>
-                        <TableCell>$250.00</TableCell>
-                        <TableCell>Categoria 1</TableCell>
-                        <TableCell>100 stock</TableCell>
-
-                        <TableCell>XS,S,L</TableCell>
-
-                        <TableCell className="text-right">
-                            <Link to='/admin/products/t-shirt-teslo'>
-                                Editar
-                            </Link>
-
-                        </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
-
-            <CustomPagination totalPages={10} />
-        </>
-    )
-}
+      <CustomPagination totalPages={data?.pages || 1} />
+    </>
+  );
+};
